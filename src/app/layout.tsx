26f5b20d6f2ad96633053b1154683b754a7c7ce0
white-duke <identifiantapple.ipad6@gmail.com>
2024-10-05 11:15:30 +0200

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,30 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Data Room Oxelta",
   description: "Oxelta, the future of gaming with Web 3. Play and earn OXLT tokens with our innovative ecosystem of play-and-earn games, Web 3, NFTs, blockchain games, crypto rewards, and game economy",
+  keywords: "data room, oxelta, web3, gaming, NFTs, blockchain, crypto",
+  openGraph: {
+    title: "Data Room Oxelta",
+    description: "Découvrez Oxelta, l'avenir du jeu avec Web 3.",
+    url: "https://data.oxelta.io/", 
+    images: [
+      {
+        url: "https://data.oxelta.io/static-site/PHOTO-2023-12-13-08-36-12.jpg",
+        alt: "Logo d'Oxelta",
+      },
+    ],
+    siteName: "Oxelta",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Data Room Oxelta",
+    description: "Découvrez Oxelta, l'avenir du jeu avec Web 3.",
+    images: [
+      {
+        url: "https://data.oxelta.io/static-site/Oxelta_P2_V2.png", 
+        alt: "Image Oxelta",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
